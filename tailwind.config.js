7ffa182bd706522734fbaee57dd85dfc0b7f2df1
RUET-CSE-IS-BEST <mdrabby30002000@gmail.com>
2024-08-06 11:42:04 +0600

    first

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "*.html",
+    "./1-2_Notes_22_Series/*.html",
+    "./Books/12-Semister/*.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
