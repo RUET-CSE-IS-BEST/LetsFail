89b3d6b7ca9c4c056dc6f3c7a66f98828f174ea9
RUET-CSE-IS-BEST <mdrabby30002000@gmail.com>
2024-08-07 15:03:35 +0600

    bg*

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        "sm-bg": "url('/Background.jpg')",
+        "sm-bg": "url('./BackGround.jpg')",
       },
       backgroundSize: {
         "bg-size": "100% 100%",
